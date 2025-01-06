449e643527c727a5297801edf00c9e0c029e9ed3
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-01-06 10:49:28 -0600

    product add form

---
+++ src/app/(private)/(dashboard)/products-add/_hooks/useProduct.tsx
@@ -0,0 +1,18 @@
+import { useMutation } from "@tanstack/react-query"
+import { useApi } from "../../../../../../lib/api"
+
+export const useProduct = () => {
+  const api = useApi();
+  
+  const addProductMutation = useMutation({
+    mutationKey: ["addProduct"],
+    mutationFn: async (formData: FormData) => {
+      const response = await api.post('/products', formData);
+      return response.data;
+    }
+  })
+
+  return {
+    addProductMutation
+  }
+}
