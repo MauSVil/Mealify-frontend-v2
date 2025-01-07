3d30a99b5f66c107ea3f154cc5b88a9dbe396a97
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-01-07 00:09:09 -0600

    fixes

--- src/app/(private)/(dashboard)/orders/list/page.tsx
+++ src/app/(private)/(dashboard)/orders/list/page.tsx
@@ -5,6 +5,7 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/
 import { useOrders } from "./_hooks/useOrders"
 import Image from "next/image";
 import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 const OrdersListPage = () => {
   const { ordersQuery } = useOrders()
@@ -12,56 +13,74 @@ const OrdersListPage = () => {
   return (
     <div className="flex flex-col gap-2 pr-10">
       <Accordion type="multiple" className="w-full">
-        {ordersQuery.data?.filter(order => order.status === 'preparing').map((order) => (
+        {ordersQuery.data
+        ?.filter(order => order.status === 'preparing')
+        ?.sort((a, b) => new Date(b.created_at!).getTime() - new Date(a.created_at!).getTime())
+        .map((order) => (
           <AccordionItem key={order.id} value={String(order.id!)} className="mb-3">
             <AccordionTrigger className="bg-gray-100/70 px-2">
               <div className="flex w-full justify-between gap-1 pr-5">
-                <span>
-                  <strong>Fecha de creación: </strong>
-                  {moment(order.created_at).format("DD/MM/YYYY")}
-                </span>
-                <span>
-                  <strong>Productos: </strong>
+                <div className="flex flex-col gap-1">
+                  <span className="font-semibold">Fecha de creación: </span>
+                  {moment(order.created_at).format("DD/MM/YYYY: HH:mm")}
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className="font-semibold">Productos: </span>
+
                   {order.order_items?.length} producto(s)
-                </span>
+                </div>
               </div>
             </AccordionTrigger>
             <AccordionContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="w-[100px]">Imagen</TableHead>
-                    <TableHead>Cantidad</TableHead>
-                    <TableHead>Nombre</TableHead>
-                    <TableHead>Precio</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {order.order_items?.map((oI) => (
-                    <TableRow key={oI.id}>
-                      <TableCell className="w-[80px] relative">
-                        <Image
-                          src={oI.products.image_min}
-                          layout="fill"
-                          objectFit="cover"
-                          alt="Product Image"
-                        />
-                      </TableCell>
-                      <TableCell className="w-28">{oI.quantity}</TableCell>
-                      <TableCell>{oI.products.name}</TableCell>
-                      <TableCell>${oI.products.price}</TableCell>
+              <>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[100px]">Imagen</TableHead>
+                      <TableHead>Cantidad</TableHead>
+                      <TableHead>Nombre</TableHead>
+                      <TableHead>Precio</TableHead>
                     </TableRow>
-                  ))}
-                </TableBody>
-                <TableFooter>
-                  <TableRow>
-                    <TableCell colSpan={3}>Total</TableCell>
-                    <TableCell className="text-right">
-                      ${order.total_price}
-                    </TableCell>
-                  </TableRow>
-                </TableFooter>
-              </Table>
+                  </TableHeader>
+                  <TableBody>
+                    {order.order_items?.map((oI) => (
+                      <TableRow key={oI.id}>
+                        <TableCell className="w-[80px] relative">
+                          <Image
+                            src={oI.products.image_min}
+                            layout="fill"
+                            objectFit="cover"
+                            alt="Product Image"
+                          />
+                        </TableCell>
+                        <TableCell className="w-28">{oI.quantity}</TableCell>
+                        <TableCell>{oI.products.name}</TableCell>
+                        <TableCell>${oI.products.price}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                  <TableFooter>
+                    <TableRow>
+                      <TableCell colSpan={3}>Total</TableCell>
+                      <TableCell className="text-right">
+                        ${order.total_price}
+                      </TableCell>
+                    </TableRow>
+                  </TableFooter>
+                </Table>
+                <div className="flex w-full justify-end">
+                  <Button
+                    variant={"success"}
+                    size={"sm"}
+                    className="mt-2"
+                    onClick={() => {
+                      console.log(order)
+                    }}
+                  >
+                    Marcar como listo
+                  </Button>
+                </div>
+              </>
             </AccordionContent>
           </AccordionItem>  
         ))}
