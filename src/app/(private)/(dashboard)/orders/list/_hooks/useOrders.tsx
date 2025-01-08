c441ea23e39f28f048f4b5e75be964d6830a7e2e
Mau S Vilchis <maudev96@gmail.com>
2025-01-07 21:01:25 -0600

    fix

--- src/app/(private)/(dashboard)/orders/list/_hooks/useOrders.tsx
+++ src/app/(private)/(dashboard)/orders/list/_hooks/useOrders.tsx
@@ -2,6 +2,7 @@ import { useMutation, useQuery } from "@tanstack/react-query"
 import { useApi } from "../../../../../../../lib/api"
 import { useBusiness } from "@/contexts/BusinessContext";
 import { Order } from "@/types/Order.type";
+import { toast } from "sonner";
 
 export const useOrders = () => {
   const api = useApi();
@@ -19,6 +20,7 @@ export const useOrders = () => {
     mutationKey: ["orders", "update"],
     mutationFn: async (order: Partial<Order>) => {
       const resp = await api.put(`/orders`, order);
+      toast.success('Se ha actualizado el estado de la orden');
       return resp.data;
     },
   })
