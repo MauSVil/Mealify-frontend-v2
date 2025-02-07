5cef2a2b65accbb362818083773bab54febf86b6
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-02-07 00:11:21 -0600

    little fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/home");
+    }, 1000); 
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Button>Click me</Button>
